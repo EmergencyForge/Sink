0148e38082beb2473a3e7ca7fa5c594c0470b239
hypax <github@hypax.wtf>
2025-12-29 05:46:33 +0100

    Adding redirect

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,3 +1,7 @@
+<script setup lang="ts">
+await navigateTo('https://emergencyforge.de', { external: true })
+</script>
+
 <template>
   <main class="flex flex-col justify-center">
     <HomeTwitter />
